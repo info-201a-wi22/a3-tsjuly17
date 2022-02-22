a9e6ad389bf6b81cadb9daedb2241ddc60a2b25c
Takumi Shimada <tsjuly171996@gmail.com>
2022-02-21 22:00:05 -0800

    working

--- source/analysis.r
+++ source/analysis.r
@@ -1,17 +1,11 @@
-incarceration <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv", stringsAsFactors = FALSE)
-
 # Load packages
 library(tidyverse)
 library(fBasics)
 library(plotly)
 library(sf)
-install.packages("devtools")
-devtools::install_github("UrbanInstitute/urbnmapr")
-
-library(urbnmapr)
 
 # Read data
-inc <- read.csv("incarceration_trends.csv", stringsAsFactors = FALSE)
+inc <- read_csv("incarceration_trends.csv")
 
 # Select variables of interest
 my_dat <- inc %>%
@@ -45,30 +39,30 @@ dat2 <- my_dat %>%
 dat2
 
 # Generate dataframe for average population by year
-df1 <- my_dat %>%
-  group_by(year) %>%
-  summarise(Avg.Pop = mean(total_jail_pop, na.rm = T))
+df1 <- inc %>%
+  dplyr::select(
+    white_jail_pop, black_jail_pop, native_jail_pop, latinx_jail_pop,
+    other_race_jail_pop, year
+  ) %>%
+  dplyr::rename(
+    White = "white_jail_pop", Black = "black_jail_pop",
+    Native = "native_jail_pop", Latin = "latinx_jail_pop",
+    Other = "other_race_jail_pop"
+  ) %>%
+  reshape2::melt(id = "year") %>%
+  dplyr::group_by(year, variable) %>%
+  summarise(Avg.Pop = mean(value, na.rm = T))
+
+
 
 # Make plot
-ggplot(df1, aes(x = year, y = Avg.Pop)) +
+ggplot(df1, aes(x = year, y = Avg.Pop, color = variable)) +
   geom_line() +
   labs(
-    x = "Year", y = "Average population",
+    x = "Year", y = "Average population", color = "Race",
     title = "Evolution of average incacerations over the years"
   ) +
   theme(plot.title = element_text(hjust = 0.5))
-
-# Scatter plot
-ggplot(my_dat, aes(
-  x = log(land_area), y = log(total_jail_pop),
-  color = urbanicity
-)) +
-  geom_point(size = 1) +
-  labs(
-    x = "Log Land area (Sq.Km)", y = "Log Total population",
-    title = "Relationsip between land area and total jail population"
-  )
-
 
 # Geographic representation
 df2 <- my_dat %>%
@@ -78,18 +72,19 @@ df2 <- my_dat %>%
 
 
 spatial_data <- left_join(df2,
-  get_urbn_map(map = "states", sf = TRUE),
-  by = "state_abbv"
+                          get_urbn_map(map = "states", sf = TRUE),
+                          by = "state_abbv"
 ) %>%
   st_as_sf()
 
 
-ggplot() +
-  geom_sf(spatial_data,
-    mapping = aes(fill = log(Avg.Pop))
+m <- ggplot() +
+  geom_sf(spatial_data, mapping = aes(fill = log(Avg.Pop))) +
+  geom_sf_text(
+    data = get_urbn_labels(map = "states", sf = TRUE),
+    aes(label = state_abbv), size = 2
   ) +
-  scale_fill_distiller("Average total jail population", palette = "Spectral") +
-  labs(
-    fill = "Log Average Population",
-    title = "Average population by State"
-  )
+  scale_fill_distiller("Log Average Population", palette = "Spectral") +
+  labs(title = "Average population by State", x = "Longitude", y = "Latitude") +
+  theme(plot.title = element_text(hjust = 0.5))
+ggplotly(m)
